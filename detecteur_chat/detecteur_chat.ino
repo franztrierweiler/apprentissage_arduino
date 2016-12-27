29ff70b1e72cd312915b1d87fb5e034256eae1e7
Franz Trierweiler <franz@MacBook-Pro-de-Franz.local>
2016-12-27 23:58:38 +0100

    Commit intial du détecteur de chat

---
+++ detecteur_chat/detecteur_chat.ino
@@ -0,0 +1,48 @@
+
+/*
+ * PIR sensor tester
+ */
+ 
+int ledPin = 13;                // choose the pin for the LED
+int inputPin = 2;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+int nombre_passages_kowloon = 0; // Nombre de fois où Kowloon est passé devant sa gamelle de croquette
+ 
+void setup() {
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+  pinMode(7,OUTPUT);
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  val = digitalRead(inputPin);  // read input value
+  //Serial.println(val);
+  
+  if (val == HIGH) {            // check if the input is HIGH
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+    if (pirState == LOW) {
+      // we have just turned on
+      Serial.println("Motion detected!");
+      // We only want to print on the output change, not state
+      pirState = HIGH;
+
+      digitalWrite(7,HIGH);
+      nombre_passages_kowloon ++;
+      Serial.println(nombre_passages_kowloon);
+    }
+  } else {
+    digitalWrite(ledPin, LOW); // turn LED OFF
+    if (pirState == HIGH){
+      // we have just turned of
+      Serial.println("Motion ended!");
+      // We only want to print on the output change, not state
+      pirState = LOW;
+      digitalWrite(7,LOW);
+  
+    }
+  }
+  
+}
+
